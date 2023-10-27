9122e3d92e353b93deee8f13623d6cbc295d1117
sachin_ghuge <ghugesachin0806@gmail.com>
2023-10-27 16:38:37 +0530

    first

---
+++ src/test/java/com/example/coders_battle/CodersBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coders_battle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodersBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
